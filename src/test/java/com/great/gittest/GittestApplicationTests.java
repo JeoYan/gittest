d320ff477f0138f148a7b676b2659979d490e404
JeoYan <215476838@qq.com>
2019-12-14 11:12:11 +0800

    Initial commit

---
+++ src/test/java/com/great/gittest/GittestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.great.gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
